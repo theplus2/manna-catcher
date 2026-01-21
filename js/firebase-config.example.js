aaba83b59b83a5f4eb97c1e688d4c648c3c623da
Pastor Yoon <theplus2@gmail.com>
2026-01-21 22:14:29 +0900

    feat: Enhance Firebase security and fix UI/UX issues

---
+++ js/firebase-config.example.js
@@ -0,0 +1,11 @@
+// [SECURITY] 이 파일은 Git에 공유되는 예제 파일입니다.
+// 실제 키 값은 'js/firebase-config.js' 파일(Git에 안 올라감)에 직접 입력해서 사용하세요.
+
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,15 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyAuYe8wA_dR4ACkcnBvaLJgVP7vdQVppGE",
-    authDomain: "mannacatcher-game.firebaseapp.com",
-    projectId: "mannacatcher-game",
-    storageBucket: "mannacatcher-game.firebasestorage.app",
-    messagingSenderId: "355134115026",
-    appId: "1:355134115026:web:9447a59fef8307a9d3310b"
+    apiKey: "AIzaSyCfUcgENySpEmM56MYqr1z8iW0pcJ6DrOQ",
+    authDomain: "manna-catcher.firebaseapp.com",
+    projectId: "manna-catcher",
+    storageBucket: "manna-catcher.firebasestorage.app",
+    messagingSenderId: "1001372054941",
+    appId: "1:1001372054941:web:dc58693505d94741c6ab74",
+    measurementId: "G-08HXEGD63T"
 };
